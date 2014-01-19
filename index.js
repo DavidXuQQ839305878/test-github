2c329eee7e4a6c83978057babd46a8f3017d5678
David <wpsite@qq.com>
2014-01-19 23:50:40 +0800

    2014-1-19

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ http.createServer(function (req, res) {
 }).listen(1337, '127.0.0.1');
 
 #test
-console.log('Server running at http://127.0.0.1:1337/');
+console.log("hihihi! ");
+console.log("hello,my world!");
+console.log("hello!hello!");
